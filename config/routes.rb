619b8778508e59a64d7d2a5e568556f7667feaee
EC2 Default User <ec2-user@ip-172-31-47-213.ap-northeast-1.compute.internal>
2023-03-02 06:05:28 +0000

    [add]routes編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,36 @@
 Rails.application.routes.draw do
+
+  root to: "public/homes#top"
+
+
+  #管理者
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+  sessions: "admin/sessions"
+  }
+
+  get 'admin' => 'admin/homes#top'
+
+  namespace :admin do
+    resources :users, only: [:index, :show, :edit, :update]
+
+    resources :post_images, only: [:index, :show, :update]
+
+    resources :post_blogs, only: [:index, :show, :update]
+  end
+
+
+
+  #ユーザー
+  devise_for :user, controllers: {
+  registrations: "pubic/registrations",
+  sessions: "pubic/sessions"
+  }
+
+  scope module: :public do
+    resources :post_images, only: [:new, :create, :index, :show, :destroy]
+
+  end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
