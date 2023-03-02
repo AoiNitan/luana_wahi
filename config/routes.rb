ae35a688727c0ff201a1152822b13b1b589c198c
EC2 Default User <ec2-user@ip-172-31-47-213.ap-northeast-1.compute.internal>
2023-03-02 08:12:45 +0000

    [add]routes編集

--- config/routes.rb
+++ config/routes.rb
@@ -27,8 +27,21 @@ Rails.application.routes.draw do
   }
 
   scope module: :public do
-    resources :post_images, only: [:new, :create, :index, :show, :destroy]
+    resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+      resource :post_images_favorites, only: [:create, :destroy]
+      resources :post_images_comments, only: [:create, :destroy]
+    end
 
+    resources :post_blogs, only: [:new, :create, :index, :show, :destroy] do
+      resource :post_blogs_favorites, only: [:create, :destroy]
+      resources :post_blogs_comments, only: [:create, :destroy]
+    end
+
+    get 'users/my_page' => "users#show"
+    get 'users/information/edit' => "users#edit"
+    patch 'users/information' => "users#update"
+    get 'users/unsubscribe' => "users#unsubscribe"
+    patch 'users/withdraw' => "users#withdraw"
   end
 
 
