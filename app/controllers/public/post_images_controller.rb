f70bff16bf1f78c9d500ba02fc13afd5462d9fc6
EC2 Default User <ec2-user@ip-172-31-47-213.ap-northeast-1.compute.internal>
2023-03-03 08:05:35 +0000

    [add]controller編集

--- app/controllers/public/post_images_controller.rb
+++ app/controllers/public/post_images_controller.rb
@@ -1,2 +1,14 @@
 class Public::PostImagesController < ApplicationController
+  before_action :authenticate_user!, except: [:show, :index]
+
+
+  def new
+    @post_image = PostImage.new
+  end
+
+  def create
+    @post_image = PostImage.new(post_image_params)
+
+
+  end
 end
