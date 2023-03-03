9b251d490b64bc31593cc46086c1535a96aed193
EC2 Default User <ec2-user@ip-172-31-47-213.ap-northeast-1.compute.internal>
2023-03-03 06:35:15 +0000

    [add]Userテーブルis_deleted追加

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,17 @@
 class Admin::UsersController < ApplicationController
+  before_action :authenticate_admin!
+
+  def index
+    @users = User.all.page(params[:page]).per(10)
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  
 end
