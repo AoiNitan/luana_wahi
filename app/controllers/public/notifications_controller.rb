598af7707d112cb85d91ac07332a79f37e8704ff
EC2 Default User <ec2-user@ip-172-31-47-213.ap-northeast-1.compute.internal>
2023-03-16 06:11:15 +0000

    [add]投稿View編集

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -1,9 +1,10 @@
 class Public::NotificationsController < ApplicationController
 
   def index
-    @notifications = current_user.passive_notifications.page(params[:page]).per(10)
+    @notifications = current_user.passive_notifications.page(params[:page]).per(20)
     @notifications.where(checked: false).each do |notifications|
-      notifications
+      notification.update_attributes(checked: true)
+    end
   end
 
 end
--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -2,6 +2,8 @@
 
 class Public::RegistrationsController < Devise::RegistrationsController
    before_action :configure_sign_up_params, only: [:create]
+   before_action :ensure_normal_user, only: [:destroy]
+
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -53,6 +55,12 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # The path used after sign up.
    def after_sign_up_path_for(resource)
     post_images_path
+   end
+
+   def ensure_normal_user
+    if resource.email == 'guest@example.com'
+      redirect_to root_path, alert: 'ゲストユーザーは削除できません。'
+    end
    end
 
   # The path used after sign up for inactive accounts.
